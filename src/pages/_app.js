36c2e9cd32241939e616bf6abcc0f1a5ac3c6436
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-04-13 08:05:26 +0200

    add animation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,10 @@ import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import DarkModeProvider from '@/context/DarkModeProvider';
 import '@/styles/globals.css';
+import { AnimatePresence } from 'framer-motion';
 import { Tajawal } from 'next/font/google';
 import Head from 'next/head';
+import { Router, useRouter } from 'next/router';
 
 const tajawal = Tajawal({
   subsets: ['latin'],
@@ -11,6 +13,7 @@ const tajawal = Tajawal({
   weight: ['400', '700', '800'],
 });
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -22,7 +25,9 @@ export default function App({ Component, pageProps }) {
           className={`${tajawal.variable} font-taj dark:bg-dark bg-light w-full min-h-screen`}
         >
           <Navbar />
-          <Component {...pageProps} />;
+          <AnimatePresence mode='wait'>
+            <Component kay={router.asPath} {...pageProps} />;
+          </AnimatePresence>
           <Footer />
         </main>
       </DarkModeProvider>
